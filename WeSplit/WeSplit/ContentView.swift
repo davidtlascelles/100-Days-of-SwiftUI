4ff1f72dee6c234551b0eb228bf0d5f82bcd504d
David Lascelles <davidtlascelles@gmail.com>
2022-01-01 15:31:39 +0200

    Completed WeSplit tutorial with challenges and personal modifications

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -11,35 +11,88 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
-    let tipPercentages = [10, 15, 20, 25, 0]
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let total = checkAmount + tipValue
+        
+        return total
+    }
     
-    let currencyCode = Locale.current.currencyCode ?? "USD"
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    let localizedCurrency: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currencyCode ?? "USD")
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", // <- Just placeholder text
-                          // Two way bound property
-                          // Using "value" instead of "text" since it is a number.
-                          value: $checkAmount,
-                          // Specify the kind of number is being provided
-                          format: .currency(
-                            // Locale struct contains all kinds of user localized preferences
-                            code: Locale.current.currencyCode ?? "USD")
-                ).keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount,
+                              format: localizedCurrency)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                } header: {
+                    Text("Enter check amount")
+                }
+                Section {
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(.menu)
+                } header: {
+                    Text("Number of people")
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.wheel)
+                     .frame(height: 95)
+                } header: {
+                    Text("Tip amount")
+                }
+                Section {
+                    Text(totalPerPerson,
+                         format: localizedCurrency
+                    )
+                } header: {
+                    Text("Ammount per person")
+                }
+                Section {
+                    Text(grandTotal, format: localizedCurrency)
+                } header: {
+                    Text("Check total")
+                }
             }
-            Section {
-                Text(checkAmount,
-                     format: .currency(code: currencyCode)
-                )
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
             }
-        }
+        }.navigationViewStyle(.stack)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
