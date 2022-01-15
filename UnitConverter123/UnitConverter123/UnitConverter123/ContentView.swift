f9f2927ae8c79e0aafb0d9473b057f1575bb5706
David Lascelles <davidtlascelles@gmail.com>
2022-01-15 18:37:59 +0200

    Completed UnitConverter123 app. (Minimum viable product). Improvements could be rounding result calculation, adding different types of units to convert, and improving text input functionality (Clear existing text when keyboard is brought up)

--- UnitConverter123/UnitConverter123/UnitConverter123/ContentView.swift
+++ UnitConverter123/UnitConverter123/UnitConverter123/ContentView.swift
@@ -7,79 +7,133 @@
 
 import SwiftUI
 
+protocol MeasurementType: CaseIterable {}
+
+enum Temperature: String, CaseIterable, MeasurementType {
+    case celcius = "°C"
+    case fahrenheit = "°F"
+    case kelvin = "°K"
+}
+
+enum Distance: String, CaseIterable, MeasurementType{
+    case meters
+    case feet
+    case yards
+    case miles
+}
+
+enum Time: String, CaseIterable, MeasurementType {
+    case seconds
+    case minutes
+    case hours
+    case days
+    case weeks
+    case months
+    case years
+}
+
+enum Volume: String, CaseIterable, MeasurementType {
+    case liters
+    case cups
+    case pints
+    case quarts
+    case gallons
+}
+
 struct ContentView: View {
-    @State private var selectedUnitCategory: UnitCategory = .temperature
+    private let initUnitString = "Initial Unit"
+    private let resultUnitString = "Result Unit"
+    //@State private var selectedUnitCategory: Unit.Category = .temperature
     @State private var selectedInitialUnit: Temperature = .celcius
     @State private var selectedResultUnit: Temperature = .fahrenheit
+    @State private var inputValue = 0.0
+    @FocusState private var inputIsFocused: Bool
     
-    enum UnitCategory: String, Equatable, CaseIterable {
-        case temperature = "Temp"
-        case distance = "Distance"
-        case time = "Time"
-        case volume = "Volume"
+    var toCelcius: Double {
+        let temp = inputValue
+        switch selectedInitialUnit {
+        case .celcius:
+            return temp
+        case .kelvin:
+            return temp - 273.15
+        case .fahrenheit:
+            return temp - 32 * 5 / 9
+        }
     }
     
-    enum Temperature: String, Equatable, CaseIterable {
-        case celcius = "°C"
-        case fahrenheit = "°F"
-        case kelvin = "°K"
+    var toKelvin: Double {
+        let temp = inputValue
+        switch selectedInitialUnit {
+        case .celcius:
+            return temp + 273.15
+        case .kelvin:
+            return temp
+        case .fahrenheit:
+            return temp - 32 * 5 / 9 + 273.15
+        }
     }
     
-    enum Distance {
-        case meters
-        case feet
-        case yards
-        case miles
+    var toFahrenheit: Double {
+        let temp = inputValue
+        switch selectedInitialUnit {
+        case .celcius:
+            return temp * 9 / 5 + 32
+        case .kelvin:
+            return temp - 273.15 * 9 / 5 + 32
+        case .fahrenheit:
+            return temp
+        }
     }
     
-    enum Time {
-        case seconds
-        case minutes
-        case hours
-        case days
-        case weeks
-        case months
-        case years
+    var outputValue: Double {
+        switch selectedResultUnit {
+        case .celcius:
+            return toCelcius
+        case .fahrenheit:
+            return toFahrenheit
+        case .kelvin:
+            return toKelvin
+        }
     }
-    
-    enum Volume {
-        case liters
-        case cups
-        case pints
-        case quarts
-        case gallons
-    }
+
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    Picker("Unit Category", selection: $selectedUnitCategory) {
-                        ForEach(UnitCategory.allCases, id: \.self) {
-                            Text("\($0.rawValue)")
-                        }
-                    }.pickerStyle(.segmented)
-                } header: {
-                    Text("Unit Category")
+                    HStack {
+                        Text("\(outputValue.formatted())\(selectedResultUnit.rawValue)" )
+                            .font(.custom("biggerTitle", size: 75, relativeTo: .largeTitle))
+                    }.frame(maxWidth: .infinity, alignment: .trailing)
                 }
                 Section {
-                    Picker("Initial Unit", selection: $selectedInitialUnit) {
-                        ForEach(Temperature.allCases, id: \.self) {
-                            Text("\($0.rawValue)")
-                        }
+                    Text("Initial Unit").listRowSeparator(.hidden)
+                    Picker(initUnitString, selection: $selectedInitialUnit){
+                        ForEach(Temperature.allCases, id: \.self) { Text("\($0.rawValue)") }
                     }.pickerStyle(.segmented)
-                } header: {
-                    Text("Initial Unit")
-                }
-                Section {
-                    Picker("Result Unit", selection: $selectedResultUnit) {
-                        ForEach(Temperature.allCases, id: \.self) {
-                            Text("\($0.rawValue)")
-                        }
-                    }.pickerStyle(.segmented)
-                } header: {
                     Text("Result Unit")
+                    Picker(initUnitString, selection: $selectedResultUnit){
+                        ForEach(Temperature.allCases, id: \.self) { Text("\($0.rawValue)") }
+                    }.pickerStyle(.segmented).listRowSeparator(.hidden)
+                }
+
+                Section {
+                    HStack {
+                        TextField("Input", value: $inputValue, format: .number)
+                            .font(.body)
+                            .multilineTextAlignment(.trailing)
+                            .keyboardType(.decimalPad)
+                            .focused($inputIsFocused)
+                    }.frame(maxWidth: .infinity, alignment: .trailing)
                 }
             }.navigationTitle("Unit Converter")
+             .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        inputIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
